6c2e5b051dd0da90815d9009038c94d6ac0795e1
hiteshlilhare <hitesh.lilhare@gmail.com>
2017-11-07 21:05:54 +0100

    Update litecoin.c

--- src/litecoin.c
+++ src/litecoin.c
@@ -1,9 +1,6 @@
 #include <sys/mman.h>
 #include <sys/stat.h>
 #include <sys/types.h>
-#include <sys/mman.h>
-#include <sys/stat.h>
-#include <sys/types.h>
 #include <errno.h>
 #include <stdio.h>
 #include <string.h>
@@ -32,12 +29,17 @@ uint32_t blk_cnt = 0;
 //lookup-Hashmap.
 std::map<std::string, struct BolckHeader> lookup_new;
 std::map<unsigned int, std::string> blkno_blkhash_new;
-
 char last_block_hash_str[HASH_LEN*2+1];
 enum parse_blk_state p_blk_s = P_BLK_MAGIC;
 enum parse_tx_state p_tx_s = P_TX_VERSION;
 enum parse_txin_state p_txin_s = P_TXIN_PREV_HASH;
 enum parse_txout_state p_txout_s = P_TXOUT_VALUE;
+//function prototype
+void reverse_byte_array(uint8_t *byte_arr,uint8_t *rev_byte_arr,int size);
+uint64_t create_block_lookup(struct block_header_hash blk_hdr, struct BolckHeader bh);
+/*
+ * Map the magic number into network enumeration
+ */
 
 enum magic_net parse_is_magic(uint32_t m)
 {
@@ -49,3 +51,8 @@ enum magic_net parse_is_magic(uint32_t m)
     }
     return mn;
 }
+
+void reverse_byte_array(uint8_t *byte_arr,uint8_t *rev_byte_arr,int size){
+	for(int i = 0; i<size;i++){		
+		rev_byte_arr[i] = byte_arr[size-1-i];
+	}
