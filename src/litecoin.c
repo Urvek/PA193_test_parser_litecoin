f67a83fdb73c3c04511211d1ff0100e4aa8360e6
hiteshlilhare <hitesh.lilhare@gmail.com>
2017-11-07 20:34:25 +0100

    Update litecoin.c

--- src/litecoin.c
+++ src/litecoin.c
@@ -1,3 +1,18 @@
 #include <sys/mman.h>
 #include <sys/stat.h>
 #include <sys/types.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <errno.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <time.h>
+#include <unistd.h>
+
+char last_block_hash_str[HASH_LEN*2+1];
+enum parse_blk_state p_blk_s = P_BLK_MAGIC;
+enum parse_tx_state p_tx_s = P_TX_VERSION;
+enum parse_txin_state p_txin_s = P_TXIN_PREV_HASH;
+enum parse_txout_state p_txout_s = P_TXOUT_VALUE;
