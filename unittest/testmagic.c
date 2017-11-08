8a9b71db6b52304a397ad2abc9cf357a188bf100
viktech123 <32435084+viktech123@users.noreply.github.com>
2017-11-08 20:52:12 +0100

    Add files via upload

---
+++ unittest/testmagic.c
@@ -0,0 +1,163 @@
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <errno.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <time.h>
+#include <unistd.h>
+#include <dirent.h>
+#include <fcntl.h>
+#include "format.h"
+#include "parse.h"
+
+#include <stdlib.h>
+#include <string.h>
+enum parse_blk_state p_blk_s = P_BLK_MAGIC;
+enum magic_net parse_is_magic(uint32_t m)
+{
+	enum magic_net mn = MAGIC_NET_NONE;
+	if (m == MAGIC_MAIN) {
+        mn = MAGIC_NET_MAIN;
+    } else if (m == MAGIC_TESTNET) {
+        mn = MAGIC_NET_TESTNET;
+    } else if (m == MAGIC_TESTNET3) {
+        mn = MAGIC_NET_TESTNET3;
+    } else if (m == MAGIC_NAMECOIN) {
+        mn = MAGIC_NET_NAMECOIN;
+    }
+    return mn;
+}
+
+void parse_block_print(struct block *b)
+{
+    
+    uint8_t i;
+    uint32_t expectedmagic;
+    uint32_t actualmagic;
+    
+    printf("Actual magic: 0x%X\n", b->magic);
+    //printf("Expected magic: 0x%X\n", b->magic);
+    printf("\n ");
+    
+    //printf("magic: 0x%X\n", b->magic);
+}
+uint64_t parse_block(uint8_t *src, uint64_t sz)
+{
+    uint8_t *p = src;
+    struct block b;
+    uint64_t skip = 0;
+    uint64_t done = 0;
+    uint64_t byte_count = 0;
+    struct BolckHeader bh;
+
+    /* Look for different patterns depending on our state */
+    while (sz > skip) 
+	{
+
+        p += skip;
+        sz -= skip;
+        byte_count += skip;
+		b.magic = *((uint32_t *)p);
+		if (parse_is_magic(b.magic) != MAGIC_NET_NONE) 
+		{
+                skip = MAGIC_LEN;
+                p_blk_s = P_BLK_SZ;
+
+            /* No magic number at this byte, check the next one */
+            } 
+			else 
+			{
+                skip = 1;                               
+            }
+        done += skip;
+    }
+
+    return done;
+}
+
+uint64_t parse(int blkfd, uint64_t sz)
+{
+    uint8_t *blk;
+    uint64_t done;
+
+    /* Map the input file */
+    blk = (uint8_t *)mmap(NULL, sz, PROT_READ, MAP_PRIVATE, blkfd, 0);
+
+    /* Process each block in this file */
+    done = parse_block(blk, sz);
+    if(done)
+    parse_block_print(&blk);
+}
+    
+int main(int argc, char *argv[])
+{
+    DIR *datadir = NULL;
+    struct dirent *dp = NULL;
+    struct stat sb;
+    int fd;
+    int ret = 0;
+
+    if (argc == 2) {
+
+        /* Open the blockchain data directory */
+        datadir = opendir(argv[1]);
+        if (datadir == NULL) {
+            perror("opendir");
+            ret = 1;
+
+        } else {
+
+            chdir(argv[1]);
+
+            do {
+                errno = 0;
+                
+                dp = readdir(datadir);
+                if (dp != NULL) {
+                    if (memcmp(dp->d_name, "blk", 3) == 0) {
+                        
+                        /* 
+                         * NOTE: We are not bothering to sort by filename because we
+                         * assume the blockchain files have ascending inode numbers due
+                         * to the way they were created. Other sources may require more
+                         * sorting.
+                         */
+                        /* printf("%d\n", dp->d_ino); */
+
+                        fd = open(dp->d_name, O_RDONLY);
+                        if (fd < 0) {
+                            perror("open");
+                        } else {
+                            ret = fstat(fd, &sb);
+                            if (ret < 0) {
+                                perror("stat");
+                            } else {
+                                /* printf("%s\n", dp->d_name); */
+                                parse(fd, (uint64_t)sb.st_size);
+                            }
+                            close(fd);
+                        }
+                    }
+                } else {
+                    if (errno != 0) {
+                        perror("readdir");
+                    }
+                }
+            } while (dp != NULL && datadir != NULL);
+
+        }
+        
+        closedir(datadir);
+
+    } else {
+        ret = 1;
+    }
+
+    if (ret != 0) {
+        printf("Usage: %s /path/to/blockchain/datadir\n", argv[0]);
+    }
+
+    return ret; 
+}
